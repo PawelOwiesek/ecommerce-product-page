8246633699da5b70920f1c270f46af4abbdd65a8
Pawel Owiesek <Owiesek.p@gmail.com>
2023-08-25 15:42:21 +0200

    add theme,ThemeProvider,GlobalStyles

--- src/index.js
+++ src/index.js
@@ -2,11 +2,17 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./core/App";
 import reportWebVitals from "./reportWebVitals";
+import { theme } from "./core/theme";
+import { ThemeProvider } from "styled-components";
+import { GlobalStyles } from "./core/GlobalStyles";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <GlobalStyles />
+      <App />
+    </ThemeProvider>
   </React.StrictMode>
 );
 
