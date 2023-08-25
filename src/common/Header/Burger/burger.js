38c0a08ac1731b2c0a60c1c90098b792bce3f40e
Pawel Owiesek <Owiesek.p@gmail.com>
2023-08-25 19:02:07 +0200

    add burger component for mobile width styles

---
+++ src/common/Header/Burger/burger.js
@@ -0,0 +1,11 @@
+import { Container, Strip } from "./styled";
+
+export const Burger = () => {
+  return (
+    <Container>
+      <Strip />
+      <Strip />
+      <Strip />
+    </Container>
+  );
+};
--- src/common/Header/index.js
+++ src/common/Header/index.js
@@ -10,10 +10,13 @@ import {
 import logo from "../../images/icons/logo.svg";
 import cart from "../../images/icons/icon-cart.svg";
 import avatar from "../../images/jpgImages/image-avatar.png";
+import { Burger } from "./Burger/burger";
 
 const Header = () => {
+  const mobile = window.innerWidth <= 768;
   return (
     <Container>
+      {mobile ? <Burger /> : null}
       <Logo src={logo} alt="logo" />
       <Navigation>
         <Item>
