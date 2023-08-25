6c0fd5048ca25889125030456f957dfe252cb09b
Pawel Owiesek <Owiesek.p@gmail.com>
2023-08-25 15:43:44 +0200

    add  theme.js

---
+++ src/core/theme.js
@@ -0,0 +1,19 @@
+export const theme = {
+  colorsPrimary: {
+    orange: "hsl(26, 100% ,55%)",
+    paleOrange: "hsl(25, 100%, 94%)",
+  },
+  neutralColors: {
+    veryDarkBlue: "hsl(220, 13%, 13%)",
+    darkGrayishBlue: "hsl(219, 9%, 45%)",
+    grayishBlue: "hsl(220, 14%, 75%)",
+    lightGrayishBlue: "hsl(223, 64%, 98%)",
+    white: "hsl(0, 0%, 100%)",
+    black: " hsl(0, 0%, 0%)",
+  },
+
+  breakpoints: {
+    mobile: "375px",
+    desktop: "1440px",
+  },
+};
