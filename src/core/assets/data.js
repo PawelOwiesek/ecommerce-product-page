33dbd61f2e1c49edc06cb51e184c0e054b290f7a
Pawel Owiesek <Owiesek.p@gmail.com>
2023-08-26 17:02:45 +0200

    created data,js with all tile info

---
+++ src/core/assets/data.js
@@ -0,0 +1,20 @@
+import snickers from "../../images/jpgImages/image-product-1.jpg";
+import snickers2 from "../../images/jpgImages/image-product-2.jpg";
+import snickers3 from "../../images/jpgImages/image-product-3.jpg";
+import snickers4 from "../../images/jpgImages/image-product-4.jpg";
+
+export const data = [
+  {
+    fall: {
+      image: [snickers, snickers2, snickers3, snickers4],
+      alt: "snickers",
+      subTitle: "sneaker company",
+      title: "Fall Limited Edition  Sneakers",
+      description:
+        " These low-profile sneakers are your perfect casual wear companion. Featuring a durable rubber outer sole, they’ll withstand everything the weather can offer.",
+      discountPrice: " $125.00",
+      discount: "50%",
+      price: "$250.00",
+    },
+  },
+];
