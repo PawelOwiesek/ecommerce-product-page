8275d1b2a36764e7d38ea7d086914b8d2fb7100c
Pawel Owiesek <Owiesek.p@gmail.com>
2023-08-29 13:36:10 +0200

    adding number of products to cart interaction

--- src/features/Main/index.js
+++ src/features/Main/index.js
@@ -1,9 +1,17 @@
 import { useState } from "react";
 import Tile from "../../common/Tile";
 import { data } from "../../core/assets/data";
-import { Counter, Item, TileList } from "./styled";
+import {
+  Button,
+  CartIcon,
+  Counter,
+  CounterButton,
+  CounterWrapper,
+  Item,
+  TileList,
+} from "./styled";
 
-const Main = () => {
+const Main = ({ counter, setCounter, addProductsToCart }) => {
   const [itemIndex, setItemIndex] = useState(0);
 
   const handleImageChange = (index) => {
@@ -24,27 +32,55 @@ const Main = () => {
     setItemIndex((index) => index + 1);
   };
 
+  const counterDecrement = () => {
+    if (!counter) {
+      return;
+    }
+    return setCounter((counter) => counter - 1);
+  };
+
+  const counterIncrement = () => {
+    return setCounter((counter) => counter + 1);
+  };
+
   return (
     <>
       {data.map((item) => {
         const path = item.fall;
         return (
-          <div key={path.image[itemIndex]}>
-            <Tile
-              itemIndex={itemIndex}
-              image={path.image[itemIndex]}
-              alt={path.alt}
-              subTitle={path.subTitle}
-              title={path.title}
-              description={path.description}
-              discountPrice={path.discountPrice}
-              discount={path.discount}
-              price={path.price}
-              handleImageChange={() => handleImageChange(itemIndex)}
-              onIndexPrevChange={() => onIndexPrevChange(itemIndex)}
-              onIndexNextChange={() => onIndexNextChange(itemIndex)}
-            />
-          </div>
+          <>
+            <div key={path.image[itemIndex]}>
+              <Tile
+                itemIndex={itemIndex}
+                image={path.image[itemIndex]}
+                alt={path.alt}
+                subTitle={path.subTitle}
+                title={path.title}
+                description={path.description}
+                discountPrice={path.discountPrice}
+                discount={path.discount}
+                price={path.price}
+                handleImageChange={() => handleImageChange(itemIndex)}
+                onIndexPrevChange={() => onIndexPrevChange(itemIndex)}
+                onIndexNextChange={() => onIndexNextChange(itemIndex)}
+              />
+            </div>
+            <CounterWrapper>
+              <Counter>
+                <CounterButton onClick={() => counterDecrement()}>
+                  -
+                </CounterButton>
+                <strong>{counter}</strong>
+                <CounterButton onClick={() => counterIncrement()}>
+                  +
+                </CounterButton>
+              </Counter>
+              <Button onClick={() => addProductsToCart()}>
+                <CartIcon />
+                Add to cart
+              </Button>
+            </CounterWrapper>
+          </>
         );
       })}
       <TileList>
@@ -55,9 +91,8 @@ const Main = () => {
               <Tile small image={image} alt={fallData.alt} />
             </Item>
           ));
-        })}
+        })}{" "}
       </TileList>
-      <Counter></Counter>
     </>
   );
 };
