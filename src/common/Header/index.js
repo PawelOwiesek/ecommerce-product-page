6917893c907fc8775b3b6a524855c0a6e85cb98a
Pawel Owiesek <Owiesek.p@gmail.com>
2023-08-29 10:16:02 +0200

    added Modal component to Header

--- src/common/Header/index.js
+++ src/common/Header/index.js
@@ -4,20 +4,27 @@ import cart from "../../images/icons/icon-cart.svg";
 import avatar from "../../images/jpgImages/image-avatar.png";
 import { Burger } from "./Navigation/Burger/burger";
 import { useState } from "react";
+import Modal from "../Modal";
 
 const Header = () => {
   const [open, setOpen] = useState(false);
+  const [showModal, setShowModal] = useState(false);
 
   const onBurgerClick = () => {
-    setOpen(!open);
+    setOpen((open) => !open);
+  };
+
+  const onCartClick = () => {
+    setShowModal((showModal) => !showModal);
   };
 
   return (
     <Container>
       <Logo src={logo} alt="logo" />
       <Burger open={open} onBurgerClick={onBurgerClick} />
-      <Cart src={cart} alt="cart" />
+      <Cart src={cart} alt="cart" onClick={() => onCartClick()} />
       <Avatar src={avatar} alt="avatar" />
+      <Modal showModal={showModal} />
     </Container>
   );
 };
