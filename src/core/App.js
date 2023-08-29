ff0bbb6bb63ccd0bd9d34d456f75b3cbded07928
Pawel Owiesek <Owiesek.p@gmail.com>
2023-08-29 13:40:55 +0200

    add state variable: counter and buyCount

--- src/core/App.js
+++ src/core/App.js
@@ -1,11 +1,30 @@
+import { useState } from "react";
 import Header from "../common/Header";
 import Main from "../features/Main";
 
 function App() {
+  const [counter, setCounter] = useState(0);
+  const [buyCount, setBuyCount] = useState(0);
+
+  const addProductsToCart = () => {
+    setBuyCount(buyCount + counter);
+    setCounter((counter) => (counter = 0));
+  };
+
   return (
     <>
-      <Header />
-      <Main />
+      <Header
+        addPrductsToCart={addProductsToCart}
+        buyCount={buyCount}
+        setBuyCount={setBuyCount}
+      />
+      <Main
+        counter={counter}
+        setCounter={setCounter}
+        addProductsToCart={addProductsToCart}
+        setBuyCount={setBuyCount}
+        buyCount={buyCount}
+      />
     </>
   );
 }
